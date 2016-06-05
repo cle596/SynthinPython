2836fb42a89c0a791fc34dd22e098629b2b3f970
changhan <changhanlee@gmail.com>
2016-06-05 00:54:41 -0400

    multithreaded

--- main.py
+++ main.py
@@ -11,17 +11,25 @@ cprint("your favorite synth looper!",'green',attrs=['bold'])
 
 from synth import *
 
+import threading
+from queue import Queue
+
 while True:
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=1)
     i=input("twang>")
     if i=="play":
+        t = threading.Thread(target=play_tone,args=(stream,C5,1,44100))
+        t.daemon = True  # thread dies when main thread (only non-daemon thread) exits.
+        t.start()
+        """
         play_tone(
             stream,
             frequency=C5,
             length=1,
             rate=44100
         )
+        """
     if i=="loop":
         x=0
         while x<8:
